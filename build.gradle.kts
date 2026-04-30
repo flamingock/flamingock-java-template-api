0ec1e309997cc1b89c865f5c0e31ee6f90b9ac22
Antonio Perez Dieppa <aperezdieppa@gmail.com>
2026-04-30 18:15:57 +0100

    chore: update version to 1.3.3

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,7 +10,7 @@ plugins {
 
 
 group = "io.flamingock"
-val declaredVersion = "1.3.2-SNAPSHOT"
+val declaredVersion = "1.3.3-SNAPSHOT"
 version = VersionManager.resolveVersion(declaredVersion, project.hasProperty("release"))
 
 repositories {
